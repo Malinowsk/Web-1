ae0bd0b3ee98bccf6dc01d5a69d3d9e3c2a5d598
Malinowsk <juanrago2015@gmail.com>
2022-06-17 15:15:13 -0300

    agrego fech get y post

--- site/js/tabla-dinamica.js
+++ site/js/tabla-dinamica.js
@@ -1,32 +1,22 @@
 document.addEventListener("DOMContentLoaded",actualizarCompraDePacks);
 
 function actualizarCompraDePacks(){
-    "use strict"
-
-    let tabla_datos = [{"usuario":"Malinowsk","email":"malinowski@gmail.com","telefono": 2494542370,
-                        "tiempo": "1 Año", "pack":"Premium"}, //fila 1
-                       {"usuario":"Ulises","email":"ulises@gmail.com","telefono": 2494542333,
-                        "tiempo": "1 Mes", "pack":"Normal"}, //fila 2
-                       {"usuario":"Cracken","email":"ck@gmail.com","telefono": 2494543030,
-                       "tiempo": "1 Año", "pack":"Premium"}, //fila 3
-                       {"usuario":"Samuel","email":"samuel80@gmail.com","telefono": 2494545099,
-                        "tiempo": "1 Año", "pack":"Bit Humanoide"}, //fila 4
-                       {"usuario":"Guillermo","email":"guille@gmail.com","telefono": 2494344579,
-                        "tiempo": "1 Mes", "pack":"Bit Humanoide"},  //fila 5
-                        {"usuario":"Arthas","email":"Arthas@gmail.com","telefono": 2391344579,
-                        "tiempo": "1 Año", "pack":"Premium"}]; //fila 6      
-               
+    "use strict";
+    
+    const url = "https://62a95d68ec36bf40bdb68e4e.mockapi.io/api/pedidoss";
+    let cantDeFilas = 0;
+    
     let id_botones = ["btn1","btn2","btn3"]; //arreglo con ids de botones submit
-
-    mostrarTabla(); // mostrar por web, filas precargadas
+    
+    obtenerTabla();
 
     let form = document.querySelector("#form");
     form.addEventListener("submit",function(e){actualizarTabla(e);}); //pongo a escuchar el evento submit del formulario
-    
+
     let reset = document.querySelector("#reset"); //boton reset
     reset.addEventListener("click", ()=>{  // pongo a escuchar el evento click del boton reset
         tabla_datos = [];   //vacio tabla
-        mostrarTabla(); // mostrar tabla por web
+        obtenerTabla(); // mostrar tabla por web
     })
 
     //funcion que toma los datos cargados en el formulario los agrega al json y actualiza tabla para mostrar por web 
@@ -50,25 +40,55 @@ function actualizarCompraDePacks(){
         while(noEncontrado){
             if( id_botones[k] == e.submitter.id ){ 
                 for (let w = 0; w <= k ; w++) {
-                    tabla_datos.push(fila_para_agregar);
+                    fila_para_agregar.id = toString(cantDeFilas+1);
+                    enviarFila(fila_para_agregar);
+                    cantDeFilas++;
                 }
                 noEncontrado=false; // corta el while
             }
             k++; // sumo indice en 1
         }
 
-        mostrarTabla(); // se muestra por pantalla la tabla de datos
         vaciarFormulario(); // resetea formulario
     }
 
+    async function enviarFila(fila){
+        try{
+            let respuesta= await fetch(url, { "method":"POST", "headers": {"Content-type": "application/json"}, "body": JSON.stringify(fila) });
+            console.log(respuesta.status);
+            if(respuesta.status === 201){
+                console.log(respuesta.status);
+                obtenerTabla();
+            } 
+            else
+                            console.log("no 202");
+        }
+        catch(errorConexion){console.log("error")};
+    }
+
+    async function obtenerTabla(){
+        
+        try{
+            let respuesta= await fetch(url);
+            if(respuesta.ok){                               
+                            let tabla = await respuesta.json(); 
+                            cantDeFilas= tabla.length;
+                            mostrarTabla(tabla);} 
+            else
+                            console.log("no 202")
+        }
+        catch(errorConexion){console.log(errorConexion)};
+    }
+
     //funcion que muestra en web la tabla almacenada en variable
-    function mostrarTabla(){
+    function mostrarTabla(tabla_datos){
         let tabla_view = document.querySelector("#tabla-body"); //selecionamos el elem tabla body
         tabla_view.innerHTML = ""; // vaciamos el elemento 
         for (const fila of tabla_datos){    // recorremos todas las filas del arreglo (tabla)
             const tr = document.createElement("tr"); // creamos un elem tr (fila)
             for (const elem in fila) { // recorremos todos los elem de la fila (json)
-                tr.innerHTML += `<td> ${fila[elem]} </td>`; // almacenamos todos los datos en el elem tr 
+                if(elem !== "id")
+                    tr.innerHTML += `<td> ${fila[elem]} </td>`; // almacenamos todos los datos en el elem tr 
             }
             if(fila.pack=="Premium" && fila.tiempo=="1 Año") // preguntamos si culple la condicion
                 tr.setAttribute("class","resaltado"); // agregamos atributo resaltado
