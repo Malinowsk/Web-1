13ab5a6a52dbfeda47c500fa7f4258a3a28d8811
Malinowsk <juanrago2015@gmail.com>
2022-06-20 20:01:32 -0300

    agrego carpeta site2 donde esta hecho opcional partial render

---
+++ site2/js/index.js
@@ -0,0 +1,294 @@
+document.addEventListener("DOMContentLoaded",partialRender);
+
+function partialRender(){
+    "use strict"
+    
+    
+    window["inicio"].addEventListener("click",(event)=>{ push(event);});
+    window["servicios"].addEventListener("click",(event)=>{ push(event)});
+    window["compras"].addEventListener("click",(event)=>{ push(event)});
+    window["contacto"].addEventListener("click",(event)=>{ push(event);});
+    
+    window.addEventListener("popstate", event => { 
+        // Tome la identificación del estado del historial
+        let id = event.state.id; 
+        seleccionarTab(id); 
+        cargarContenido(id);
+    });
+
+    cargarInicio();
+    
+    function cargarInicio(){
+        let id = "inicio";
+        seleccionarTab(id); //colorea boton de navegacion selecionado
+        
+        document.title = "Bit Humanoide - " + id;
+        cargarContenido(id);
+        window.history.pushState({ id },`${id}`,`/${id}`);
+
+    }
+
+    function push(event){
+        let id = event.target.id;
+        seleccionarTab(id); //colorea boton de navegacion selecionado
+        
+        document.title = "Bit Humanoide - " + id;
+        cargarContenido(id);
+        window.history.pushState({ id },`${id}`,`/${id}`);
+
+    }
+    
+    function seleccionarTab(id){
+        document.querySelectorAll(".navegacion").forEach((item)=> item.classList.remove("pag-actual")); 
+        document.querySelector(`#${id}`).classList.add("pag-actual");
+    }
+    
+    async function cargarContenido(id){
+        let contenedor = document.querySelector("#main");
+        try{
+            let respuesta = await fetch(`${window.location.origin}/${id}.html`);
+            if(respuesta.ok){
+                let contenido = await respuesta.text();
+                contenedor.innerHTML = contenido;
+                if(id==="contacto"){
+                    iniciarCaptcha();
+                }
+                if(id==="compras"){
+                    actualizarCompraDePacks();
+                }
+                
+            }
+            else{
+                contenedor.innerHTML = "error 202";
+            }
+
+        }
+        catch(error){
+            contenedor.innerHTML = "error";
+        }
+
+        function capitalizarPrimeraLetra(str) {
+            return str.charAt(0).toUpperCase() + str.slice(1);
+          }
+    }
+
+    // funcion utilizada para compras.html
+    function actualizarCompraDePacks(){
+        
+        const url = "https://62a95d68ec36bf40bdb68e4e.mockapi.io/api/pedidoss"; 
+        
+        obtenerTabla();
+        
+        let form = document.querySelector("#form"); //formulario
+        form.addEventListener("submit",function(e){  //pongo a escuchar el evento submit del formulario
+            actualizarTabla(e);
+        });
+    
+        let reset = document.querySelector("#reset"); //boton reset
+        reset.addEventListener("click", function(){  // pongo a escuchar el evento click del boton reset
+            tabla_datos = [];   //vacio tabla
+            obtenerTabla(); // mostrar tabla por web
+        });
+     
+        function actualizarTabla(e){
+    
+            e.preventDefault(); // captura el evento del envio de formulario para que no se refresque la web
+    
+            let formData = new FormData(form); // se crea variable que contiene datos cargados en el formulario
+            let fila_para_agregar = {}; // se crea fila json
+            
+            //se cargan los datos del formulario en la nueva fila 
+            fila_para_agregar.usuario =formData.get("usuario"); 
+            fila_para_agregar.email=formData.get("email");
+            fila_para_agregar.telefono=formData.get("telefono");
+            fila_para_agregar.tiempo=formData.get("tiempo");
+            fila_para_agregar.pack=formData.get("pack");
+            
+            let id_botones = ["btn1","btn2","btn3"]; //arreglo con ids de botones submit
+            let noEncontrado=true;
+            let k=0; // indice que recorre el arreglo botones submits
+            while(noEncontrado){
+                if( id_botones[k] == e.submitter.id ){ 
+                        enviarFila(fila_para_agregar,k+1);
+                        noEncontrado=false;
+                }
+                k++; // sumo indice en 1
+            }
+    
+            vaciarFormulario(); // resetea formulario
+        }
+    
+        async function enviarFila(fila,cant){
+            try{
+                let respuesta= await fetch(url, {   "method":"POST", 
+                                                    "headers": {"Content-type": "application/json"}, 
+                                                    "body": JSON.stringify(fila) });
+    
+                if(respuesta.status === 201){
+                    if(cant==1)
+                        obtenerTabla();
+                    else
+                    {
+                        enviarFila(fila,cant-1);
+                    }
+                } 
+                else
+                    console.log("no 201");
+            }
+            catch(errorConexion){console.log("error")};
+        }
+    
+        async function obtenerTabla(){
+            
+            try{
+                let respuesta= await fetch(url);
+                if(respuesta.ok){                               
+                    let tabla = await respuesta.json(); 
+                    mostrarTabla(tabla);} 
+                else
+                    console.log("no 202")
+            }
+            catch(errorConexion){console.log(errorConexion)};
+        }
+    
+        //funcion que muestra en web la tabla almacenada en variable
+        function mostrarTabla(tabla_datos){
+            let tabla_view = document.querySelector("#tabla-body"); //selecionamos el elem tabla body
+            tabla_view.innerHTML = ""; // vaciamos el elemento 
+            for (const fila of tabla_datos){    // recorremos todas las filas del arreglo (tabla)
+                let id;
+                const tr = document.createElement("tr"); // creamos un elem tr (fila)/blogs?page=1&limit=10
+                for (const elem in fila) { // recorremos todos los elem de la fila (json)
+                    if(elem !== "id")
+                        tr.innerHTML += `<td>${fila[elem]}</td>`; // almacenamos todos los datos en el elem tr 
+                    else
+                        id=fila[elem];
+                }
+                tr.innerHTML += `<td><button id="borrar-${id}"><i class="fa fa-trash-alt"></i></button><button id="editar-${id}"><i class="fa fa-pencil-alt"></i></button><button id="confirmar-${id}">Confirmar</button></td>`;
+                if(fila.pack=="Premium" && fila.tiempo=="1 Año") // preguntamos si culple la condicion
+                    tr.setAttribute("class","resaltado"); // agregamos atributo resaltado
+                tabla_view.appendChild(tr); // agregamos fila al elem html (tabla)
+                document.querySelector(`#borrar-${id}`).addEventListener("click",function(){borrarFila(id)});
+                
+                document.querySelector(`#editar-${id}`).addEventListener("click",function(){editarFila(this)});
+                document.querySelector(`#confirmar-${id}`).addEventListener("click",function(){confirmarFila(this,id)});
+                document.querySelector(`#confirmar-${id}`).classList.add("invisibilidad");
+            }
+        }
+    
+        async function borrarFila(id){
+            try{
+                let respuesta= await fetch(`${url}/${id}`, { "method":"DELETE"});
+                if(respuesta.status === 200){
+                    console.log(respuesta.status);
+                    obtenerTabla();
+                } 
+                else
+                    console.log("no 202");
+            }
+            catch(errorConexion){console.log("error")};
+        }
+    
+        function editarFila(boton){
+            let filaAEditar = boton.parentElement.parentElement;
+            let arregloDeDatos = filaAEditar.children;
+            for(let i = 0; i < arregloDeDatos.length-1; i++){
+                let input = document.createElement("input");
+                input.value=arregloDeDatos[i].innerHTML;
+                arregloDeDatos[i].innerHTML="";
+                arregloDeDatos[i].appendChild(input);
+            }
+            boton.classList.add("invisibilidad");
+            boton.nextElementSibling.classList.remove("invisibilidad");
+        }
+    
+        function confirmarFila(boton,id){
+            let filaAEditar = boton.parentElement.parentElement;
+            let arregloDeDatos = filaAEditar.children;
+           
+            let fila_modificada = {}; // se crea fila json
+    
+            
+            fila_modificada.usuario =arregloDeDatos[0].firstElementChild.value; 
+            fila_modificada.email=arregloDeDatos[1].firstElementChild.value;
+            fila_modificada.telefono=arregloDeDatos[2].firstElementChild.value;
+            fila_modificada.tiempo=arregloDeDatos[3].firstElementChild.value;
+            fila_modificada.pack=arregloDeDatos[4].firstElementChild.value;
+    
+            guardarCambiosFila(fila_modificada, id);
+            boton.innerHTML="Espere";
+            
+        }
+    
+        async function guardarCambiosFila(fila,id){
+            try{
+                let respuesta= await fetch(`${url}/${id}`, {   "method":"PUT", 
+                                                    "headers": {"Content-type": "application/json"}, 
+                                                    "body": JSON.stringify(fila) });
+    
+                if(respuesta.status === 200)
+                    obtenerTabla();
+                else
+                    console.log("no 201");
+            }
+            catch(errorConexion){console.log("error")};
+        }
+    
+    
+        function vaciarFormulario(){
+            form.reset();
+        }
+    }
+
+    function iniciarCaptcha(){
+
+        let resultado; // variable para guardar el valor valido del captcha
+        generarCaptcha();
+        document.querySelector("#validar").addEventListener("click",validarCaptcha);//pongo a escuchar el evento para validar el captcha
+        
+        
+        //genera el captcha para validar que es un humano
+        function generarCaptcha(){
+        let primer_numero = generarNumero(); // genero numero aleatorio
+        let segundo_numero = generarNumero(); // genero numero aleatorio 
+        document.querySelector("#prim_num").innerHTML = primer_numero; // agrego prim num en etiqueta span
+        document.querySelector("#seg_num").innerHTML = segundo_numero; // agrego seg num en etiqueta span
+        resultado= primer_numero + segundo_numero; // guardo resultado valido del captcha
+        }
+        
+        function generarNumero(){
+        return Math.floor(Math.random()*10); //devuelve un numero entero entre 0 y 9
+        }
+        
+        //funcion para validar si el valor puesto para el captcha es el correcto o no
+        function validarCaptcha(){
+        if(document.querySelector("#valor-captcha").value==resultado){
+            let contenedor = document.querySelector("#captcha");
+            contenedor.innerHTML="";
+            contenedor.classList.remove("captcha");
+            document.querySelector("#oculta").checked = true; // confirma que el captcha fue validado para poder enviar el formulario
+            informarResultado(true);
+        }
+        else{
+            informarResultado(false);
+        }
+        }
+        
+        //funcion que muestra por pantalla mensaje sobre la resolucion del captcha   
+        function informarResultado(valido){ 
+        let parrafo = document.querySelector("#info-resolucion");
+        if (valido){
+            parrafo.classList.add("mensaje-valido");
+            parrafo.classList.remove("mensaje-invalido");
+            parrafo.innerHTML = "Validación satisfactoria!";
+        }
+        else
+        {
+            parrafo.classList.add("mensaje-invalido");
+            parrafo.innerHTML = "La validación se efectuó incorrectamente!";
+        }
+        }
+    }
+    
+
+}
